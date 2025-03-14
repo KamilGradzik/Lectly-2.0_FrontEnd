f52404022706d834f51e6a61e1d0bda144531f89
KamilGradzik <k.gradzik97@gmail.com>
2025-03-15 00:05:40 +0100

    Initial commit. Organizing project's structure.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+
+const App = ():JSX.Element => {
+  return (
+    <div className="App">
+      <h1>Application Set Up!</h1>
+    </div>
+  );
+}
+
+export default App;
