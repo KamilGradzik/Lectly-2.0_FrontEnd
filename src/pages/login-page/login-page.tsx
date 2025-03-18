668c53c2e8b44412b7ec227e1b08a069f7e9d291
KamilGradzik <k.gradzik97@gmail.com>
2025-03-18 16:26:38 +0100

    Working on login page, added necessary fields and buttons and stlized them.

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,17 +1,31 @@
-import { TextField } from "@mui/material"
+import { Button, Divider, FormControl, Link, TextField } from "@mui/material"
 import { JSX } from "react"
 import "./login-page.scss"
 
 const LoginPage = ():JSX.Element => {
     return(
         <div className="login-page">
-            <div className="login-page-logo">
-                <p>Login Placeholder</p>
-            </div>
-            <div className="login-page-form">
-                <div className="login-page-form-wrapper">
-                    <TextField variant={"filled"} label={"E-mail"} />
-                    <TextField variant={"filled"}  label={"Password"} />
+            <div className="login-container">
+                <div className="login-container-sub">
+                    <div className="login-page-logo">
+                        <p className="logo-title">Lectly</p>
+                        <p className="logo-subtitle">Supports teachers work!</p>
+                    </div>
+                </div>
+                <Divider />
+                <div className="login-container-sub">
+                    <div className="login-page-form">
+                        <FormControl className="login-page-form-field">
+                            <TextField  variant="outlined" label="E-mail" type="email" autoComplete="off" />
+                        </FormControl>
+                        <FormControl className="login-page-form-field">
+                            <TextField variant="outlined"  label="Password" type="password" />
+                        </FormControl>
+                        <Link href="#">Forgot password</Link>
+                        <FormControl className="login-page-form-field">
+                            <Button variant={"contained"} type={"submit"} >LOG IN</Button>
+                        </FormControl>
+                    </div>
                 </div>
             </div>
         </div>
